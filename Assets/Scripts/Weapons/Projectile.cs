85f093bff7c1ef8f6c556fe25726f9d3fccb8ef4
RossRH <ross@renderheads.com>
2018-09-23 01:12:05 +0200

    shooting

---
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	public GameObject OnDestroyParticleSystem;
+
+	[Space]
+	public float Speed;
+	public LevelManager LevelManager;
+	
+	void Update ()
+	{
+		Vector3 movement = transform.forward * Speed;
+		Vector3 position = transform.position;
+		LevelManager.SnapMovementToRadius(ref position, ref movement);
+		transform.position = position + movement;
+	}
+
+	private void OnTriggerEnter(Collider coll)
+	{
+		if(OnDestroyParticleSystem != null)
+		{
+			GameObject.Instantiate(OnDestroyParticleSystem);
+		}		
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Weapons/SoundGun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundGun : Weapon
+{
+
+	float intensitySpeedMultiplier = 5;
+
+	public override void Fire(Vector3 direction, float intensity)
+	{
+		GameObject projectileObject = Instantiate(Projectile.gameObject);
+		Projectile proj = projectileObject.GetComponent<Projectile>();
+		proj.LevelManager = levelManager;
+		proj.Speed = intensity * intensitySpeedMultiplier;
+	}
+
+	public void Update()
+	{
+		UpdateAim();
+	}
+}
