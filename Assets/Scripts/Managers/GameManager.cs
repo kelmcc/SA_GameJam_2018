d73e53518110ec81c0b748a8847503bf6eecb3dc
RossRH <ross@renderheads.com>
2018-09-22 05:08:15 +0200

    Got manual bpm based timing working

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public BeatManager BeatManager;
+	public UIRoot UIRoot;
+
+	void Start ()
+	{
+		BeatManager.OnBeat += OnBeat;
+	}
+
+	void OnBeat ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/ScriptableObjects/AudioSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="AudioSettings", menuName ="BeatDetection/AudioSettings")]
+public class AudioSettings : ScriptableObject
+{
+	[Serializable]
+	public class AudioClipData
+	{
+		public AudioClip clip;
+
+		[Space]
+		//Only one song will be used for beat detection
+		public bool IsPrimary;
+		public float GThreshhold;
+		public int BufferSize;
+	}
+
+	[Serializable]
+	public class Song
+	{
+		public AudioClipData[] clips;
+		
+		[Space]
+		public float Bpm;
+		public float StartBeatDelaySeconds;
+	}
+
+	public Song[] Songs;
+}
