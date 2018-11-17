7ee3f29543bacb1133ab4b3bd82aecde586578a4
RossRH <ross@renderheads.com>
2018-11-17 17:12:45 +0200

    Hello

---
+++ Assets/ShakeOnHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeOnHit : MonoBehaviour
+{
+	private Animator animator;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		animator.SetTrigger("Shake");
+	}
+}
