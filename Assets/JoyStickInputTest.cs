0a6bbe1bf0a5c773bd7a7dcb53a9028485a429fe
Kelly McCarter <35454029+KellyMAM@users.noreply.github.com>
2018-11-26 23:02:38 +0200

    tests

---
+++ Assets/JoyStickInputTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStickInputTest : MonoBehaviour {
+
+    Vector3 startpos;
+    Transform thisTransform;
+    Vector3 inputDirection;
+
+    void Start () {
+        thisTransform = this.transform;
+        startpos = thisTransform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        inputDirection = Vector3.zero;
+        inputDirection.x = Input.GetAxis("AimHorizontal");
+        inputDirection.y = Input.GetAxis("AimVertical");
+        Debug.Log(inputDirection);
+        thisTransform.position = inputDirection;
+    }
+}
